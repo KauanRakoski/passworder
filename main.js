2c160b784f1cd8e654772120fd3d6c69f2ad7f41
Kauan Rakoski <kauan.k.rakoski@gmail.com>
2022-05-15 16:50:51 -0300

    basic style

---
+++ main.js
@@ -0,0 +1,14 @@
+const length = document.getElementById('length')
+const symbols = document.getElementById('symbols')
+const numbers = document.getElementById('numbers')
+const letters = document.getElementById('letters')
+
+const generator = document.getElementById('generator')
+
+
+const generate = () => {
+    alert('Comming soon!')
+}
+
+generator.addEventListener('click', generate)
+
